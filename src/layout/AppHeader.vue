a27cfa7d93af5a6cf01518269112bbe2557ee0f9
Higor Chagas <42949902+higorfchagas@users.noreply.github.com>
2020-09-17 19:43:14 -0300

    removelogoantigo

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -8,9 +8,7 @@
 
             <div class="row" slot="content-header" slot-scope="{closeMenu}">
                 <div class="col-6 collapse-brand">
-                    <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/">
-                        <img src="img/brand/blue.png">
-                    </a>
+                    AMS
                 </div>
                 <div class="col-6 collapse-close">
                     <close-button @click="closeMenu"></close-button>
